48de42ddde4e0a3be390b24e309708798cb86fd8
Renaud Redron <renaud.redron@gmail.com>
2024-05-21 17:01:14 +0200

    sprint3-ticket3-feat: Gestion des articles partie 2

--- src/Controller/Admin/Post/PostController.php
+++ src/Controller/Admin/Post/PostController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller\Admin\Post;
 
+use App\Entity\Post;
+use App\Entity\User;
+use DateTimeImmutable;
+use App\Form\AdminPostFormType;
 use App\Repository\PostRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,17 +26,47 @@ class PostController extends AbstractController
     #[Route('/post/list', name: 'admin_post_index', methods: ["GET"])]
     public function index(): Response
     {
-        return $this->render('pages/admin/post/index.html.twig');
+        return $this->render('pages/admin/post/index.html.twig', [
+            "posts" => $this->postRepository->findAll()
+        ]);
     }
 
     #[Route('/post/create', name: 'admin_post_create', methods: ["GET", "POST"])]
-    public function create(): Response
+    public function create(Request $request): Response
     {
 
-        
+        $post = new Post();
 
+        $form = $this->createForm(AdminPostFormType::class, $post);
 
-        return $this->render('pages/admin/post/create.html.twig');
+        $form->handleRequest($request);
+
+        if ( $form->isSubmitted() && $form->isValid())
+        {
+
+            /**
+             * Récupérons l'utilisateur connecté
+             * 
+             * @var User
+             */
+
+            $user = $this->getUser();
+            $post->setUser($user);
+
+            $post->setUpdatedAt( new DateTimeImmutable());
+            $post->setCreatedAt( new DateTimeImmutable());
+
+            $this->entityManager->persist($post);
+            $this->entityManager->flush($post);
+
+            $this->addFlash("success","L'article a été créé et sauvegardé.");
+
+            return $this->redirectToRoute("admin_post_index");
+        }
+
+        return $this->render('pages/admin/post/create.html.twig', [
+            "form" => $form->createView()
+        ]);
     }
 
 }
